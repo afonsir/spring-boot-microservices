5ed466d3e2afa3d8cc156539af5568b66334091a
AfonSir <afonsir17@gmail.com>
2022-01-28 23:47:11 -0300

    Feat: add basic user controller

---
+++ mobile-app-ws/src/main/java/com/github/afonsir/app/ws/ui/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.github.afonsir.app.ws.ui.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+  @GetMapping()
+  public String getUsers(
+    @RequestParam(value = "page", defaultValue = "1") int page,
+    @RequestParam(value = "limit", defaultValue = "50") int limit,
+    @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort
+  ) {
+    return "GET user was called with page " + page + " and limit " + limit;
+  }
+
+  @GetMapping(path = "/{userId}")
+  public String getUser(@PathVariable String userId) {
+    return "GET user was called with userId = " + userId;
+  }
+
+  @PostMapping
+  public String createUser() {
+    return "CREATE user was called";
+  }
+
+  @PutMapping
+  public String updateUser() {
+    return "UPDATE user was called";
+  }
+
+  @DeleteMapping
+  public String deleteUser() {
+    return "DELETE user was called";
+  }
+}
